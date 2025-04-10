141c23107916cb27c63f38d7bb68dbbb424d451f
Артем Волков <deman.hihi@gmail.com>
2025-04-10 08:38:13 +0300

    Revert "add CountryName"
    This reverts commit 6c2af989cbd3a75c2cc6c470729ba9283156769c.

--- git/Hotel.cs
+++ git/Hotel.cs
@@ -11,7 +11,6 @@ namespace git
         public int Id { get; set; }
         public string Name { get; set; }
         public int CountOfStars { get; set; }
-        public string CountryName { get; set; }
 
     }
 }
